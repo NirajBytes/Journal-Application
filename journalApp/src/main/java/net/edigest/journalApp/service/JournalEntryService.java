f1786f80003fc5cd8f575668548f1aeeeb3bafd1
NIraj Balpande <nirajbalpande12@gmail.com>
2025-03-13 22:20:22 +0530

    Service class annotation explain

--- journalApp/src/main/java/net/edigest/journalApp/service/JournalEntryService.java
+++ journalApp/src/main/java/net/edigest/journalApp/service/JournalEntryService.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-@Service
+@Service //to identify this class as service class by compiler
 public class JournalEntryService {
     private final JournalEntryRepository journalEntryRepository;
     private final UserService userService;
